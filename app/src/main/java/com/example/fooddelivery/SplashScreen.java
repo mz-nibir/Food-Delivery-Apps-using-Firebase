15b9fee683e149fa04c946d26248110d3198ae14
Nibir <mz.nibir@gmail.com>
2020-10-29 21:23:12 +0600

    hander added, delay 3s

--- app/src/main/java/com/example/fooddelivery/SplashScreen.java
+++ app/src/main/java/com/example/fooddelivery/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.fooddelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
@@ -11,6 +13,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        long delayMilli;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class ));
+                finish();
+            }
+        }, delayMilli=3000);
+
         Toast.makeText(this, "Splash Screen Started", Toast.LENGTH_SHORT).show();
         
     }
